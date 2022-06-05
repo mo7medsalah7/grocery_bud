106347d6a79f688892b7b7fe2947e0d15ea8f8b5
mo7medsalah7 <msmido9@gmail.com>
2022-06-05 11:14:58 +0200

    add light color to Uses link

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -22,8 +22,11 @@ function Navbar() {
       <PortalWithState closeOnOutsideClick closeOnEsc>
         {({ openPortal, closePortal, isOpen, portal }) => (
           <React.Fragment>
-            <button onClick={openPortal} className="underline">
-              Usage
+            <button
+              onClick={openPortal}
+              className="underline  text-[1.9rem] text-white"
+            >
+              Uses
             </button>
             {portal(
               <div
